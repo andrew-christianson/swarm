20958ae37b0b4bc259368c58a7ec55bf196d77b4
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1996-10-02 04:51:46 +0000

    This commit was manufactured by cvs2svn to create tag 'Swarm-961002'.

---
+++ tags/Swarm-961002/swarm/src/simtools/simtools.h
@@ -0,0 +1,20 @@
+// Swarm library. Copyright (C) 1996 Santa Fe Institute.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.
+
+#import <simtools/global.h>
+#import <simtools/ControlPanel.h>
+#import <simtools/ProbeDisplay.h>
+#import <simtools/SimpleProbeDisplay.h>
+#import <simtools/CompleteProbeDisplay.h>
+#import <simtools/CompleteProbeDisplay.h>
+#import <simtools/ProbeDisplayManager.h>
+#import <simtools/GUISwarm.h>
+#import <simtools/UName.h>
+#import <simtools/InFile.h>
+#import <simtools/OutFile.h>
+#import <simtools/ObjectLoader.h>
+#import <simtools/ObjectSaver.h>
+#import <simtools/ActiveGraph.h>
+#import <simtools/ActiveOutFile.h>

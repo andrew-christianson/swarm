ce28b9b1e7315c433000112a94d9eb755653dca5
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1997-02-02 23:54:29 +0000

    This commit was manufactured by cvs2svn to create tag 'swarm-1_0_0'.

---
+++ tags/swarm-1_0_0/swarm/src/analysis/Averager.h
@@ -0,0 +1,27 @@
+// Swarm library. Copyright (C) 1996 Santa Fe Institute.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.
+
+#import <swarmobject/MessageProbe.h>
+
+// Average object: calculates a few basic statistics given a collection of 
+// objects to poll and a selector with which to poll them.
+
+@interface Averager : MessageProbe {
+  double total;				    // total
+  double max, min;			    // maximum, minimum
+  int count;				    // total number
+  id collection;  		            // collection to average over
+}
+
+-setCollection: (id) aCollection;
+-createEnd;		
+
+-update;					  // update the average.
+-(double) getAverage;
+-(double) getTotal;
+-(double) getMax;
+-(double) getMin;
+-(int) getCount;
+@end

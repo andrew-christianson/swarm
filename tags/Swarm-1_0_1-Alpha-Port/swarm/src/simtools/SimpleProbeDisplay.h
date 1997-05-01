86fdbe932b814c41ba45a15c5da156a0509bc2e5
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1997-05-01 16:35:01 +0000

    This commit was manufactured by cvs2svn to create tag 'Swarm-1_0_1-Alpha-Port'.

---
+++ tags/Swarm-1_0_1-Alpha-Port/swarm/src/simtools/SimpleProbeDisplay.h
@@ -0,0 +1,42 @@
+// Swarm library. Copyright (C) 1996 Santa Fe Institute.
+// This library is distributed without any warranty; without even the
+// implied warranty of merchantability or fitness for a particular purpose.
+// See file LICENSE for details and terms of copying.
+
+#import <swarmobject/SwarmObject.h>
+#import <tkobjc.h>
+#import <swarmobject/ProbeMap.h>
+#import <simtools/VarProbeWidget.h>
+#import <simtools/MessageProbeWidget.h>
+
+@interface SimpleProbeDisplay : SwarmObject {
+  id probedObject;  
+  ProbeMap * probeMap;
+  Frame *topFrame, *leftFrame, *rightFrame,
+        *middleFrame, *bottomFrame, *topLevel;
+  Label *myTitle ;
+  int numberOfProbes;
+  id *widgets;
+  ref_t objectRef;
+  BOOL removeRef;
+}
+
+-setProbedObject: anObject;
+-setProbeMap: (ProbeMap *) probeMap;
+-createEnd;
+
+-getProbedObject;
+-getProbeMap;
+-update;
+
+-(char *) package ;
+-(const char *) getId ;
+
+-(void) setRemoveRef: (BOOL) torf;
+-(void) setObjectRef: (ref_t) or;
+-(void) drop;
+
+@end
+
+
+

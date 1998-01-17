0dfa37910a772bdc6fb7143ea359aae002792349
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-01-17 02:46:03 +0000

    This commit was manufactured by cvs2svn to create tag 'bank-0_3'.

---
+++ tags/bank-0_3/apps/objc/bank/FNet.m
@@ -0,0 +1,16 @@
+#define __USE_FIXED_PROTOTYPES__  // for gcc headers
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#import <tkobjc.h>
+
+#import "FNet.h"
+
+@implementation FNet
+
+@end
+
+
+

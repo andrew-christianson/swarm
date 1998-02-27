d69d34606bc8fffbb8c26cfa988c6bc09b776864
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-02-27 01:01:51 +0000

    This commit was manufactured by cvs2svn to create tag 'GraphLib-1_1'.

---
+++ tags/GraphLib-1_1/apps/objc/graph/DiGraphNode.h
@@ -0,0 +1,39 @@
+// Copyright (C) 1995-1998 The Santa Fe Institute.
+// No warranty implied, see LICENSE for terms.
+
+#import <objectbase.h>
+#import <gui.h>
+
+@interface DiGraphNode: SwarmObject
+{
+  id fromList;
+  id toList;
+  id <Canvas> canvas;
+  id <NodeItem> nodeItem;
+  id nodeType;
+  const char *label;
+  id uRandPosition;
+}
+
+- setRandPosFunc: rp;
+- setCanvas: aCanvas;
+- setCanvas: aCanvas withRandPosFunc: posFunc;
+- createEnd;
+- getNodeItem;
+- getToLinks;
+- getFromLinks;
+- makeLinkTo: aNode;
+- makeLinkFrom: aNode;
+- addFrom: aLink;
+- addTo: aLink;
+- removeFrom: aLink;
+- removeTo: aLink;
+- (int)linkedTo: anObj;
+- (int)linkedFrom: anObj;
+- (int)agreeX: (int)x Y: (int)y;
+- updateLinks;
+- hideNode;
+- (void)drop;
+- setNodeLabel: (const char *)aLabel;
+
+@end

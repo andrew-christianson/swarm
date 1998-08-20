b498d00e91d347e34f5df7fd48e783bf3cd4e9c2
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-08-20 18:31:57 +0000

    This commit was manufactured by cvs2svn to create tag 'Tutorial-1_3'.

---
+++ tags/Tutorial-1_3/apps/objc/tutorial/simpleSwarmBug3/ModelSwarm.h
@@ -0,0 +1,31 @@
+// ModelSwarm.h					SimpleBug App
+
+#import "Bug.h"
+#import "FoodSpace.h"
+#import <objectbase/Swarm.h>
+#import <space.h>
+
+@interface ModelSwarm: Swarm
+{
+  int worldXSize, worldYSize;
+  float seedProb;
+  float bugDensity;
+
+  FoodSpace *food;
+  id <Grid2d> world;
+  Bug *reportBug;
+
+  id bugList;
+  id modelActions;
+  id modelSchedule;
+}
+
++ createBegin: aZone;
+- createEnd;
+- buildObjects;
+- buildActions;
+- activateIn: swarmContext;
+
+@end
+
+

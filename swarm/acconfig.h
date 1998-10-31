c3522678851be9236588d6d2d80672dec8c9c82e
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1998-10-31 00:34:37 +0000

    This commit was manufactured by cvs2svn to create tag 'Mousetrap-1_4'.

--- swarm/acconfig.h
+++
@@ -1,9 +0,0 @@
-#undef PACKAGE
-#undef VERSION
-#undef USE_AVCALL
-#undef POINTER_FMT_HEX_PREFIX
-#undef BUGGY_BUILTIN_APPLY
-#undef HAVE_OBJC_MALLOC
-#undef DATADIR
-#undef SYSCONFDIR
-#undef PREFIX

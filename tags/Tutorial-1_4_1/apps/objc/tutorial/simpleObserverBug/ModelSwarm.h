1431e93f99e72e75f7474b95887af69ac6ee813e
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-01-17 01:43:09 +0000

    This commit was manufactured by cvs2svn to create tag 'Tutorial-1_4_1'.

---
+++ tags/Tutorial-1_4_1/apps/objc/tutorial/simpleObserverBug/ModelSwarm.h
@@ -0,0 +1,34 @@
+// ModelSwarm.h					SimpleBug App
+
+#import "Bug.h"
+#import "FoodSpace.h"
+#import <objectbase/Swarm.h>
+#import <space.h>
+
+@interface ModelSwarm: Swarm
+{
+  int worldXSize, worldYSize;
+  float seedProb;
+  float bugDensity;
+
+  FoodSpace *food;
+  id <Grid2d> world;
+
+  id bugList;
+  id modelActions;
+  id modelSchedule;
+}
+
+- getWorld;
+- getFood;
+- getBugList;
+
++ createBegin: aZone;
+- createEnd;
+- buildObjects;
+- buildActions;
+- activateIn: swarmContext;
+
+@end
+
+
---
+++ tags/Tutorial-1_4_1/apps/objc/tutorial/simpleSwarmBug/Bug.h
@@ -0,0 +1,24 @@
+// Bug.h					SimpleBug
+
+#import <objectbase/SwarmObject.h>
+
+@interface Bug: SwarmObject
+{
+  int xPos, yPos;
+  int worldXSize, worldYSize;
+  
+  id foodSpace;
+}
+
+- setWorldSizeX: (int)x Y: (int)y;
+- setFoodSpace: f;
+- createEnd;
+
+- setX: (int)x Y: (int)y;
+
+- step;
+
+@end
+
+
+

c1986475832a3451173016c0086aa207e75c2754
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-08 07:24:58 +0000

    This commit was manufactured by cvs2svn to create tag 'graph-2_0_1'.

---
+++ tags/graph-2_0_1/apps/objc/graph/graph.h
@@ -0,0 +1,9 @@
+#import <graph/DiGraph.h>
+#import <graph/DiGraphNode.h>
+#import <graph/DiGraphLink.h>
+
+#define DEFAULT_RANDOM_SEED    911
+
+extern id <Symbol> RectangleNode, OvalNode ; 
+extern void initGraphLibrary();
+

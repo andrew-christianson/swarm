f073c38b8b77a37d4b67e57cecf719da1a72492f
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-25 21:11:21 +0000

    This commit was manufactured by cvs2svn to create tag 'Bank-2_0'.

---
+++ tags/Bank-2_0/apps/objc/bank/FNet.h
@@ -0,0 +1,9 @@
+// Copyright © 1996-1999 Santa Fe Institute.
+#import <simtools.h>
+#import <graph.h>
+
+@interface FNet: DiGraph
+{
+}
+
+@end
---
+++ tags/Bank-2_0/apps/objc/bank/FNet.m
@@ -0,0 +1,9 @@
+// Copyright © 1996-1999 Santa Fe Institute.
+#import "FNet.h"
+
+@implementation FNet
+
+@end
+
+
+

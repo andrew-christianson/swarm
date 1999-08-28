817ea7faa1d5198cc951b46d18499969776ac991
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
1999-08-28 22:44:11 +0000

    This commit was manufactured by cvs2svn to create tag 'Swarm-2_0'.

---
+++ tags/Swarm-2_0/swarm/src/defobj/JavaProxy.h
@@ -0,0 +1,6 @@
+#import <defobj.h>
+#import <defobj/Create.h>
+
+@interface JavaProxy: CreateDrop
+- (unsigned)isJavaProxy;
+@end
---
+++ tags/Swarm-2_0/swarm/src/defobj/JavaProxy.m
@@ -0,0 +1,8 @@
+#include "JavaProxy.h"
+
+@implementation JavaProxy
+- (unsigned)isJavaProxy
+{
+  return 1;
+}
+@end

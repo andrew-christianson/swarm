61a8e12c272235bb018ebc795bf448885351f5e1
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-02-29 01:02:17 +0000

    This commit was manufactured by cvs2svn to create tag 'Mousetrap-2_1'.

--- swarm/java/swarm/NonUniqueMethodSignatureException.java
+++
@@ -1,9 +0,0 @@
-// Swarm library. Copyright © 1999-2000 Swarm Development Group.
-// This library is distributed without any warranty; without even the
-// implied warranty of merchantability or fitness for a particular purpose.
-// See file LICENSE for details and terms of copying.
-
-package swarm;
-
-public class NonUniqueMethodSignatureException extends Exception {
-}

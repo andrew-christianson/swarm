910061e960d460ea1ca82954bae68d17488a2029
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2000-04-02 16:08:01 +0000

    This commit was manufactured by cvs2svn to create tag 'Swarmdocs-2_1_1'.

--- apps/objc/tutorial/simpleSwarmBug3/bug.scm
+++
@@ -1,8 +0,0 @@
-(list 
- (cons 'modelSwarm
-       (make-instance 'ModelSwarm
-                      #:worldXSize 80
-                      #:worldYSize 80
-                      #:seedProb 0.9F0
-                      #:bugDensity 0.01F0)))
-

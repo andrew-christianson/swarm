e877705a0e5b8fee67e13939db5e173620acee1c
nima <nima@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2007-08-27 12:05:27 +0000

    Completed `moving' all content of all main() functions under Xcode group tests/activity/main/ into SwarmOSX_testActivity.
    Replaced all instances of `fprintf(stferr, ...); return 1' with the appropriate STAssert*/STFail method call.
    
    Had to comment out vital part of the code to stop otest form abnormally exiting with code 138, (ie crashing). Where this has been done, STFail("FIXME: ...") call has been made to make this very obvious.
    
    Now that the move has been made, the next step is to consolidate some code into the setUp(), but more importantly to uncomment the vital calls and find out why they are breaking the otest call.

--- branches/apple_port-branch/swarm/tests/activity/SwarmOSX_testActivity.m
+++ branches/apple_port-branch/swarm/tests/activity/SwarmOSX_testActivity.m
@@ -11,87 +11,367 @@
 
 @implementation SwarmOSX_testActivity
 
-- (void) test_ForEach {
+- (void) setUp {
 	argc = 1;
-  argv[0] = "dummy";
+  argv[0] = "./swarm";
+	argv[1] = "";
+	ok = YES;
 	
+	//. FIXME
+	STFail(@"FIXME: `initSwarmBatch(argc, argv);'"); //initSwarmBatch(argc, argv);
+}
+
+
+
+- (void) test_ForEach {
 	id actionGroupTest;
 	id obj;
 	id collection;
-	int i, ok;
-			
-	initSwarmBatch (argc, argv);
-	initModule (tactivity);
-	init_tables ();
-			
-	actionGroupTest = [ActionGroup_test createBegin: globalZone 
-																	numberOfObjects: 6];
-	for (i = 0; i < 5; i++)
-	{
+	int i;
+
+	//. FIXME
+	STFail(@"FIXME: `initModule(tactivity);'"); //initModule(tactivity);
+	STFail(@"FIXME: `init_tables();'"); //init_tables();
+
+	actionGroupTest = [ActionGroup_test createBegin: globalZone numberOfObjects: 6];
+	for(i=0; i<5; i++)	{
 		obj = [Responder create:globalZone];
 		[actionGroupTest addObject: obj];
 	}
-	collection = [List createBegin: globalZone];
+	collection = [List createBegin:globalZone];
 	collection = [collection createEnd];
-	for (i = 1; i <= 5; i++)
-	{
-		[collection addLast: [Responder create: globalZone withId: i]];
+	
+	for(i=1; i<=5; i++) {
+		//. FIXME
+		STFail(@"FIXME: `[collection addLast: [Responder create: globalZone withId: i]];'");
+		//[collection addLast: [Responder create: globalZone withId: i]];
 	}
+	
 	[actionGroupTest addObject: collection];
-			
 	[actionGroupTest setDefaultOrder: Sequential];
 	actionGroupTest = [actionGroupTest createEnd];
-			
-
-	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 0]
-													message: M(m1)];
-	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 1]
-													message: M(m2)];
-	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 2]
-													message: M(m3)];	
-	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 3]
-													message: M(m4)];	
-	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 4]
-													message: M(m5)];	
-	[actionGroupTest createActionForEach: [actionGroupTest getObjectAt: 5]
-															 message: M(mId)];  
+		
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 0] message: M(m1)];
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 1] message: M(m2)];
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 2] message: M(m3)];	
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 3] message: M(m4)];	
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 4] message: M(m5)];	
+	[actionGroupTest createActionForEach: [actionGroupTest getObjectAt: 5] message: M(mId)];  
+	
 	[[actionGroupTest activateIn: nil] run];
-
-	ok = 1;
-	for (i = 0; i<5; i++) 
-	{
-		/*
-		if (!messages[i]) 
-		{
-			fprintf (stderr, "Error in ActionGroupRandomized  method m%d not called !\n", i + 1);
-			return 1;
-		}
-		*/
-		//FIXME: Why is it `i+1' (should be `i')?...
-		STAssertEquals(messages[i], 0, @"Error in ActionGroupRandomized method m%d not called!\n", i + 1);
-		/*
-		if (!ids[i])
-		{
-			fprintf (stderr, "Error in ForEachAction message not sent to member of \n collection at index %d !\n", i);
-			return 1;
-		}*/
-		STAssertEquals(messages[i], 0, @"Error in ForEachAction message not sent to member of \n collection at index %d!\n", i);
-		if (ids[i] != i + 1) 
-			ok = 0;
+	 
+	for(i=0; i<5; i++) {
+		//. XXX: Why is it `i+1' (should be `i')?...
+		if(!messages[i]) { STFail(@"Error in ActionGroupRandomized method m%d not called!", i+1); }
+		if(!ids[i]) { STFail(@"Error in ForEachAction message not sent to member of "
+												  "collection at index %d!", i); }
+		if(ids[i] != i + 1) ok=NO;
 	}
-  /*
-	if (ok)
-		return 0;
-	else 
-	{
-		fprintf (stderr, "Error ForEachAction should not be randomized!\n");
-		return 1;
-	}
-	*/
-	STAssertEquals(ok, 0, @"Error ForEachAction should not be randomized!\n");
+	STAssertTrue(ok, @"Error ForEachAction should not be randomized!");
 }
 
-- (void) test_ActivityGroup {
-}		
+
+
+- (void) test_ForEachRandomized {
+	id actionGroupTest;
+  id obj;
+  id collection;
+  int i;
+  id forEachAction;
+  
+	//. FIXME
+	STFail(@"FIXME: `initModule(tactivity);'"); //initModule(tactivity);
+	STFail(@"FIXME: `init_tables();'"); //init_tables();
+	
+  actionGroupTest = [ActionGroup_test createBegin:globalZone numberOfObjects: 6];
+  for (i=0; i<5; i++) {
+		obj = [Responder create:globalZone];
+		[actionGroupTest addObject: obj];
+	}
+  collection = [List createBegin: globalZone];
+  collection = [collection createEnd];
+  for (i=1; i<=5; i++) {
+		//[collection addLast: [Responder create: globalZone withId: i]];
+		STFail(@"FIXME: `[collection addLast: [Responder create: globalZone withId: i]];'");
+	}
+  
+	[actionGroupTest addObject: collection];
+  
+  [actionGroupTest setDefaultOrder: Sequential];
+  actionGroupTest = [actionGroupTest createEnd];
+  
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 0] message: M(m1)];
+  [actionGroupTest createActionTo: [actionGroupTest getObjectAt: 1] message: M(m2)];
+  [actionGroupTest createActionTo: [actionGroupTest getObjectAt: 2] message: M(m3)];	
+  [actionGroupTest createActionTo: [actionGroupTest getObjectAt: 3] message: M(m4)];
+  [actionGroupTest createActionTo: [actionGroupTest getObjectAt: 4] message: M(m5)];
+  forEachAction = [actionGroupTest createActionForEach:[actionGroupTest getObjectAt:5]
+																							 message: M(mId)];  
+  [forEachAction setDefaultOrder: Randomized];
+  
+  [[actionGroupTest activateIn: nil] run];
+  
+	//. XXX: This logic is broken...
+  ok=NO;
+  for(i=0; i<5; i++) {
+		if(!messages[i]) { STFail(@"Error in ActionGroup method m%d not called!", i+1); }
+		if (!ids[i]) { STFail(@"Error in ForEachAction message not sent to member of "
+													 "collection at index %d!", i); }
+		if (ids[i] != i+1) ok=YES;
+	}
+  STAssertTrue(ok, @"Error ForEachAction should be randomized!");	
+}
+
+
+
+- (void) test_ActionGroup {
+	id actionGroupTest;
+	id obj;
+	int i;
+
+	//. FIXME
+	STFail(@"FIXME: `initModule(tactivity);'"); //initModule(tactivity);
+	STFail(@"FIXME: `init_tables();'"); //init_tables();
+	
+	actionGroupTest = [ActionGroup_test createBegin:globalZone numberOfObjects:5];
+	for(i=0; i<5; i++) {
+		obj = [Responder create:globalZone];
+		[actionGroupTest addObject: obj];
+	}
+	[actionGroupTest setDefaultOrder: Sequential];
+	actionGroupTest = [actionGroupTest createEnd];
+		
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 0] message: M(m1)];
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 1] message: M(m2)];
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 2] message: M(m3)];	
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 3] message: M(m4)]; 
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 4] message: M(m5)];
+	[[actionGroupTest activateIn: nil] run];
+	
+	for (i=0; i<5; i++) {
+		if (!messages[i]) { STFail(@"Error in ActionGroup method m%d not called!", i+1); }
+		if (messages[i] != i + 1) ok=NO;
+	}
+		
+	STAssertTrue(ok, @"Error ActionGroup should not be randomized!");
+}
+
+
+
+- (void) test_ActionGroupRandomized {
+	id actionGroupTest;
+  id obj;
+  int i;
+	
+	//. FIXME
+	STFail(@"FIXME: `initModule(tactivity);'"); //initModule(tactivity);
+	STFail(@"FIXME: `init_tables();'"); //init_tables();
+	
+  actionGroupTest = [ActionGroup_test createBegin: globalZone numberOfObjects: 5];
+  for (i=0; i<5; i++) {
+		obj = [Responder create:globalZone];
+		[actionGroupTest addObject: obj];
+	}
+	
+  [actionGroupTest setDefaultOrder: Randomized];
+  actionGroupTest = [actionGroupTest createEnd];
+  
+	[actionGroupTest createActionTo: [actionGroupTest getObjectAt: 0] message: M(m1)];
+  [actionGroupTest createActionTo: [actionGroupTest getObjectAt: 1] message: M(m2)];
+  [actionGroupTest createActionTo: [actionGroupTest getObjectAt: 2] message: M(m3)];	
+  [actionGroupTest createActionTo: [actionGroupTest getObjectAt: 3] message: M(m4)]; 
+  [actionGroupTest createActionTo: [actionGroupTest getObjectAt: 4] message: M(m5)]; 
+  
+  [[actionGroupTest activateIn: nil] run];
+  
+	//. XXX: This logic is broken...
+  ok=NO;
+  for (i=0; i<5; i++) {
+		if(!messages[i]) { STFail(@"Error in ActionGroupRandomized method m%d not called!", i+1); }
+		if(messages[i] != i+1) ok=YES;
+	}
+  
+  STAssertTrue(ok, @"Error ActionGroupRandomized should be randomized!");
+}
+
+
+
+- (void) test_ConcurrentGroup {
+  id concGroupTest;
+  id obj;
+  int i;
+	
+	//. FIXME
+	STFail(@"FIXME: `initModule(tactivity);'"); //initModule(tactivity);
+	STFail(@"FIXME: `init_tables();'"); //init_tables();
+	
+  concGroupTest = [ConcurrentGroup_test createBegin: globalZone numberOfObjects: 5];
+  for (i=0; i<5; i++)	{
+		obj = [Responder create:globalZone];
+		[concGroupTest addObject: obj];
+	}
+  [concGroupTest setDefaultOrder: Sequential];
+  concGroupTest = [concGroupTest createEnd];
+
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 0] message: M(m1)];
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 1] message: M(m2)];
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 2] message: M(m3)];	
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 3] message: M(m4)]; 
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 4] message: M(m5)]; 
+  [[concGroupTest activateIn: nil] run];
+	
+  for (i=0; i<5; i++) {
+		if(!messages[i]) { STFail(@"Error in ConcurrentGroup method m%d not called!", i+1); }
+		if(messages[i] != i + 1) ok=NO;
+	}
+  
+	STAssertTrue(ok, @"Error ConcurrentGroup should not be randomized!");
+}
+
+
+
+- (void) test_ConcurrentGroupRandomized {
+	id concGroupTest;
+  id obj;
+  int i;
+  
+	//. FIXME
+	STFail(@"FIXME: `initModule(tactivity);'"); //initModule(tactivity);
+	STFail(@"FIXME: `init_tables();'"); //init_tables();
+  
+  concGroupTest = [ConcurrentGroup_test createBegin:globalZone numberOfObjects:5];
+  for(i=0; i<5; i++) {
+		obj = [Responder create:globalZone];
+		[concGroupTest addObject: obj];
+	}
+	
+  [concGroupTest setDefaultOrder: Randomized];
+  concGroupTest = [concGroupTest createEnd];
+  
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 0] message: M(m1)];
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 1] message: M(m2)];
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 2] message: M(m3)];	
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 3] message: M(m4)]; 
+  [concGroupTest createActionTo: [concGroupTest getObjectAt: 3] message: M(m5)]; 
+  [[concGroupTest activateIn: nil] run];
+  
+	//. XXX: This logic is broken...
+  ok=NO;
+  for(i=0; i<5; i++) {
+		if(!messages[i]) { STFail(@"Error in ActionGroupRandomized method m%d not called!", i+1); }
+		if(messages[i] != i+1) ok=YES;
+	}
+  
+  STAssertTrue(ok, @"Error ActionGroupRandomized should be randomized!");
+}
+
+
+
+- (void) test_DynamicSchedule {
+	id theSwarm;
+	
+	//. FIXME
+	STFail(@"FIXME: `theSwarm = [DSSwarm create:globalZone];'"); //theSwarm = [DSSwarm create:globalZone];
+	STFail(@"FIXME: `[theSwarm buildActions];'"); //[theSwarm buildActions];
+	STFail(@"FIXME: `[theSwarm activateIn: nil];'"); //[theSwarm activateIn:nil];
+	STFail(@"FIXME: `[[theSwarm getActivity] run];'"); //[[theSwarm getActivity] run];
+	
+	ok=NO;
+	STAssertTrue(ok, @"Error in Schedule, dynamic update failed, action added "
+	                  "after the current time, but before first pending action "
+										"in the Schedule not performed!");
+}
+
+
+
+- (void) test_DynamicScheduleRepeat {
+	id theSwarm;
+  
+	//. FIXME
+	STFail(@"FIXME: `theSwarm = [DSRSwarm create:globalZone];'"); //theSwarm = [DSRSwarm create:globalZone];
+	STFail(@"FIXME: `[theSwarm buildActions];'"); //[theSwarm buildActions];
+	STFail(@"FIXME: `[theSwarm activateIn: nil];'"); //[theSwarm activateIn:nil];
+	STFail(@"FIXME: `[[theSwarm getActivity] run];'"); //[[theSwarm getActivity] run];
+  
+  if(stimes[0] != 1) STFail(@"Error dynamic update failed, action added after "
+														 "current time but before first action in the schedule "
+														 "never was performed!");
+
+  if(stimes[1] != 11) STFail(@"Error dynamic update failed, action added in previous "
+														  "repeat cycle never performed!");
+	
+  if(stimes[2] != 15) STFail(@"Error dynamic update failed, action added in previous "
+														  "repeat cycle before current time, never performed in "
+														  "last repeat cycle!");
+
+  if(stimes[3] != 21) STFail(@"Error dynamic update failed, action added two repeat "
+														  "cycles ago, that was performed in previous cycle never "
+														  "performed in last repeat cycle!");
+
+  if (stimes[4] != 25) STFail(@"Error dynamic update failed, action added before current "
+															 "time two repeat cycles ago, that was performed in "
+															 "previous cycle never performed in last repeat cycle");
+}
+
+
+
+- (void) test_Schedule {
+	id array;
+  id obj;
+  int i;
+  id schedule;
+  
+	//. FIXME
+	STFail(@"FIXME: `init_tables();'"); //init_tables();
+
+  array = [Array create: globalZone setCount: 5];
+  for (i=0; i<5; i++) {
+		obj = [Responder create:globalZone];
+		[obj setId: i + 1];
+		[array atOffset: i put: obj];
+	}
+	
+  schedule = [Schedule createBegin: globalZone];
+  [schedule setAutoDrop: YES];
+  schedule = [schedule createEnd];
+  
+  [schedule at: 0 createActionTo: [array atOffset: 0] message: M(mTimeId)];
+  [schedule at: 1 createActionTo: [array atOffset: 0] message: M(mTimeId)];
+  [schedule at: 1 createActionTo: [array atOffset: 1] message: M(mTimeId)];
+  [schedule at: 10 createActionTo: [array atOffset: 2] message: M(mTimeId)];	
+  [schedule at: 10 createActionTo: [array atOffset: 3] message: M(mTimeId)]; 
+  [schedule at: 100 createActionTo: [array atOffset: 4] message: M(mTimeId)];
+  [schedule at: 100 createActionTo: [array atOffset: 4] message: M(mTimeId)];
+  [[schedule activateIn: nil] run];
+  
+  if(ids[0] != 1)
+		STFail(@"Error in Schedule, action scheduled for time: 0 not performed!");
+	
+	if (ids[1] != 1 || ids[2] != 2 || rtimes[1] != 1 || rtimes[2] != 1)
+		STFail(@"Error in Schedule, two concurrent actions scheduled for time: 1 not performed!");
+  
+	if (ids[3] != 3 || ids[4] != 4 || rtimes[3] != 10 || rtimes[4] != 10)
+		STFail(@"Error in Schedule, two concurrent actions scheduled for time: 10 not performed!");
+  
+	if (ids[5] != 5 || ids[6] != 5 || rtimes[5] != 100 || rtimes[6] != 100)
+		STFail(@"Error in Schedule, two concurrent actions scheduled for time: 100 not performed!");
+}
+
+
+/*
+- (void) test_ScheduleEmpty {
+	id theSwarm;
+
+	//. FIXME
+  //theSwarm = [ESSwarm create: globalZone];
+  //[theSwarm buildActions];
+  //[theSwarm activateIn: nil];
+  //[[theSwarm getActivity] run];
+  
+	ok=NO;
+	STAssertTrue(ok, @"Error in Schedule, action added to an empty schedule never "
+							      "performed, although setKeepEmpty was set to YES!");
+}
+*/
 
 @end

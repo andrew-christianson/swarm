f5199f883b5da4e30c1ff3e102276e26ba9cf79d
nobody <nobody@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2008-04-30 02:31:30 +0000

    This commit was manufactured by cvs2svn to create branch 'objc_abstraction-branch'.

--- contrib/drugwar/graph/graph.h
+++
@@ -1,9 +0,0 @@
-#import "DiGraph.h"
-#import "DiGraphNode.h"
-#import "DiGraphLink.h"
-
-#define DEFAULT_RANDOM_SEED    911
-
-extern id <Symbol> RectangleNode, OvalNode ; 
-extern void initGraphLibrary();
-

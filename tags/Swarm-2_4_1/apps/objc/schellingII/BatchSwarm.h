58958113243f37ae4e59ddaaca7d6d9b6e02db7d
schristley <schristley@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2011-03-04 18:03:23 +0000

    2.4.1 release tag

---
+++ tags/Swarm-2_4_1/apps/objc/schellingII/BatchSwarm.h
@@ -0,0 +1,38 @@
+#import <objectbase.h>
+#import <objectbase/Swarm.h>
+#import <space.h>
+#import <analysis.h>
+#import <collections.h>
+#import <simtools.h>
+#import "ModelSwarm.h"
+#import "Parameters.h"
+
+@interface BatchSwarm : Swarm 
+{
+  int displayFrequency;				
+  int experimentDuration;
+  
+  id displayActions;				
+  id displaySchedule;
+  
+  ModelSwarm * modelSwarm;	 
+  id <Colormap> colormap;
+  id <ZoomRaster> worldRaster;
+  id <Object2dDisplay> worldDisplay;
+  id <EZGraph> moveGraph;
+
+}
+
+// Methods overriden to make the Swarm.
++ createBegin: (id)aZone;
+- createEnd;
+- buildObjects;
+- buildActions;
+- (id <Activity>)activateIn: (id)swarmContext;
+- go;
+- checkToStop;
+- stopRunning;
+
+- (void)drop;
+
+@end

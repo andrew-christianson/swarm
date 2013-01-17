2dffcd3b79cb29062ab3ee966b1ed22b853592fc
schristley <schristley@84d386b0-d280-4341-8980-c4cb7e1eb7a5>
2013-01-17 21:06:15 +0000

    updated to libtool 2.4.2

--- trunk/swarm/autogen.sh
+++ trunk/swarm/autogen.sh
@@ -19,7 +19,7 @@ AMDIR=
 
 # Update whenever version dependencies of developer tools change
 REQUIRED_AUTOCONF_VERSION="2.62"
-REQUIRED_LIBTOOL_VERSION="2.2.6b"
+REQUIRED_LIBTOOL_VERSION="2.4.2"
 REQUIRED_AUTOMAKE_VERSION="1.10.1"
 
 ("$ACDIR"autoconf --version) < /dev/null > /dev/null 2>&1 || {
